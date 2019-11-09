aed6d311ddad1d26aaed48b03bef9629979cdb40
Kishor Naik <kishor.naik011.net@outlook.com>
2019-11-09 19:26:46 +0530

    Code Commit

---
+++ Sol_NonAction/Sol_NonAction/Controllers/DemoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sol_NonAction.Models;
+
+namespace Sol_NonAction.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // Run in Browser (It Will Execute)
+        //http://localhost:26043/Demo/GetUserData
+
+        [NonAction] // Once the define NonAction Attribute in Action Method it will not execute the action
+        public List<UserModel> GetUserData()
+        {
+            return new List<UserModel>()
+            {
+                new UserModel()
+                {
+                    FullName="Kishor"
+                },
+                new UserModel()
+                {
+                    FullName="Mahesh"
+                }
+            };
+        }
+
+    }
+}
